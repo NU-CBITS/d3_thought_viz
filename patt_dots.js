a1319c63ec70231b1e691c0a385d0962510bd04c
gwensmu <gwen.smuda@gmail.com>
2014-05-16 16:55:33 -0500

    attempted to filter thoughts by pattern

--- patt_dots.js
+++ patt_dots.js
@@ -51,7 +51,6 @@ var thoughts =  [
     var lies = _.where(thoughts, {thought_pattern:'Lies'}).length;
 
     var patternFrequencies = [parseInt(allOrNothing), parseInt(catastrophic), parseInt(lies)];
-    //var patternNames = ["All or nothing", "Catastrophic", "lies"];
     
     var frequency = d3.scale.ordinal()
         .range = [parseInt(allOrNothing), parseInt(catastrophic), parseInt(lies)];
@@ -60,11 +59,19 @@ var thoughts =  [
         .range(["#98abc5", "#8a89a6", "#7b6888"]);
      
     var patternNames = d3.scale.ordinal()
-        .range(["All or nothing", "Catastrophic", "lies"]);
+        .range(['All-Or-Nothing Thinking', 'Catastrophic', 'Lies']);
         
     var margin = {top: 200, right: 30, bottom: 50, left: 200},
         width = 900 - margin.left - margin.right,
         height = 500 - margin.top - margin.bottom;
+        
+    // add the record area to the webpage
+    var record = d3.select("body").append("ul")
+        .attr("class", "record")
+        
+     // add the title area to the webpage
+    var title = d3.select("body").append("h1")
+        .attr("class", "title")
 
     // setup "x" 
     var xScale = d3.scale.linear().range([0, width]), // value -> display
@@ -88,16 +95,31 @@ var thoughts =  [
         .attr("r", (function(d) {
             return d * 50;}))
         .style("fill", (function(d, i) {
-            return color(i);}))
-         
-        // cx and cy will affect dot position, variable based on dot size need to work out
+            return color(i) }))
+            
+         // cx and cy will affect dot position, variable based on dot size need to work out
         .attr("cx", (function(d, i) {
-            return ((200 * i) + (d * 20));
+            return ((300 * i) + (d * 10));
             }))
         .attr("cy", (function(d, i) {
-            return i * 16;
-            }));
-     
+            return d * 16;}))
+            
+        .on("click", function(d) {             
+           title.html((function(d, i) {
+                                return patternNames(i) }) )
+           var patternFilter = _.where(thoughts, {thought_pattern : (function(d, i) {
+                                                                            return patternNames(i);}) })
+           record.html(
+             _.each(patternFilter, function(el, idx){
+                $(".record").append('<li>' + '<h3>' + el.thought + '</h3>' + '</li>');
+                }));
+            });
+        /*.on("mouseout", function(d) {
+             title.transition()
+                   .duration(500)
+                   .style("opacity", 0); */
+        
+        
     var text = svg.selectAll("text")
         .data(patternFrequencies)
         .enter()
@@ -105,13 +127,31 @@ var thoughts =  [
         
         // x and y need to equal cx and cy above
         .attr("x", (function(d, i) {
-            return ((200 * i) + (d * 20));
+            return ((300 * i) + (d * 10));
             }))
         .attr("y", (function(d, i) {
-            return i * 16;
+            return d * 16;
             }))
         .attr("dy", ".3em")
         .style("text-anchor", "middle")
         .text((function(d, i) {
             return patternNames(i);
             }));
+            
+              
+  /*  var thoughts = svg.selectAll("record")
+        .data(patternFrequencies)
+        .enter()
+        .append("text")
+        .text(function() {
+          _.each(thoughts, function(el, idx){
+            $(".title").append(el.thought_pattern);
+            $(".record").append('<li>' + '<h3>' + el.thought + '</h3>' + '</li>');
+            })
+        });
+  window.onload = function() {
+      _.each(thoughts, function(el, idx){
+        $(".title").append(el.thought_pattern);
+        $(".record").append('<li>' + '<h3>' + el.thought + '</h3>' + '</li>');
+        });
+    }; */
