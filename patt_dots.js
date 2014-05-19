fbf16f67412f0f6d32e4a32f326c193dc2c70f66
gwensmu <gwen.smuda@gmail.com>
2014-05-19 13:43:54 -0500

    On hover, details about thought patterns appear
    Fixed mouseover behavior, list correct thought patterns, added
    placeholder for recommendation, revised dot positioning.

--- patt_dots.js
+++ patt_dots.js
@@ -30,7 +30,7 @@ var thoughts =  [
                         USER:'023342', 
                         id:3, 
                         thought:'I’ll never be good at playing players', 
-                        thought_pattern: 'Lies',
+                        thought_pattern: 'Falsehoods',
                         challenging_thought:'How do i know until I try?',
                         as_if:'I’m going to practice more'
                     },
@@ -48,30 +48,43 @@ var thoughts =  [
     // thought patterns, to be replaced with actual patt names
     var allOrNothing = _.where(thoughts,{thought_pattern:'All-Or-Nothing Thinking'}).length;
     var catastrophic = _.where(thoughts, {thought_pattern:'Catastropic'}).length;
-    var lies = _.where(thoughts, {thought_pattern:'Lies'}).length;
+    var falsehoods = _.where(thoughts, {thought_pattern:'Falsehoods'}).length;
 
-    var patternFrequencies = [parseInt(allOrNothing), parseInt(catastrophic), parseInt(lies)];
+    // used to build dot size
+    var patternFrequencies = [parseInt(allOrNothing), parseInt(catastrophic), parseInt(falsehoods)];
     
-    var frequency = d3.scale.ordinal()
-        .range = [parseInt(allOrNothing), parseInt(catastrophic), parseInt(lies)];
+    /* var frequency = d3.scale.ordinal()
+        .range = [parseInt(allOrNothing), parseInt(catastrophic), parseInt(falsehoods)]; */
 
+    // color of dot, index matches patternNames
     var color = d3.scale.ordinal()
         .range(["#98abc5", "#8a89a6", "#7b6888"]);
      
+    // name of pattern to label dot and connect dot graphic to object collections on mouseover
     var patternNames = d3.scale.ordinal()
-        .range(['All-Or-Nothing Thinking', 'Catastrophic', 'Lies']);
+        .range(['All-Or-Nothing Thinking', 'Catastrophic', 'Falsehoods']);
         
-    var margin = {top: 200, right: 30, bottom: 50, left: 200},
-        width = 900 - margin.left - margin.right,
+    // clinician recommendation to be indexed here; index will need to correlate to index of patternNames  
+    var recommendations = d3.scale.ordinal()
+        .range(['See shades of grey', 'Take a deep breath', 'Challenge your assumptions']);
+        
+    // margin of the SVG that contains dots
+    var margin = {top: 100, right: 30, bottom: 50, left: 200},
+        width = 600 - margin.left - margin.right,
         height = 500 - margin.top - margin.bottom;
         
-    // add the record area to the webpage
+    /* add the record area to the webpage
     var record = d3.select("body").append("ul")
         .attr("class", "record")
         
      // add the title area to the webpage
     var title = d3.select("body").append("h1")
-        .attr("class", "title")
+        .attr("class", "title") */
+        
+    // add the mouseover details area to the webpage
+    var tooltip = d3.select("body").append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 0);
 
     // setup "x" 
     var xScale = d3.scale.linear().range([0, width]), // value -> display
@@ -82,10 +95,8 @@ var thoughts =  [
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
         .append("g")
+        .attr("id", "svg")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-       
-    // don't want dots overlapping axis, so add in buffer to data domain
-        //xScale.domain([d3.min(patternFrequencies, xValue)-1, d3.max(patternFrequencies, xValue)+1]);
             
     // draw dots
      svg.selectAll(".dot")
@@ -93,32 +104,34 @@ var thoughts =  [
         .attr("class", "dot")
         .enter().append("circle")
         .attr("r", (function(d) {
-            return d * 50;}))
+            return d * 30;}))
         .style("fill", (function(d, i) {
             return color(i) }))
             
-         // cx and cy will affect dot position, variable based on dot size need to work out
+         // cx and cy will affect dot position, variable based on dot size, will need to refactor for all 12 categories
         .attr("cx", (function(d, i) {
-            return ((300 * i) + (d * 10));
+            return ((50 * i) + (d * 10));
             }))
         .attr("cy", (function(d, i) {
-            return d * 16;}))
-            
-        .on("click", function(d) {             
-           title.html((function(d, i) {
-                                return patternNames(i) }) )
-           var patternFilter = _.where(thoughts, {thought_pattern : (function(d, i) {
-                                                                            return patternNames(i);}) })
-           record.html(
-             _.each(patternFilter, function(el, idx){
-                $(".record").append('<li>' + '<h3>' + el.thought + '</h3>' + '</li>');
-                }));
-            });
-        /*.on("mouseout", function(d) {
-             title.transition()
-                   .duration(500)
-                   .style("opacity", 0); */
+            return i * 150;}))
         
+        .on("mouseover", function(d, i) {
+            tooltip.transition()
+               .duration(200)
+               .style("opacity", .9)
+            tooltip.html('<h1> Examples of ' + patternNames(i) + ': <h1>')
+            var recordSet = _.where(thoughts, {thought_pattern : patternNames(i)})
+            _.each(recordSet, function(el, idx){
+                $(".tooltip").append('<li>' + '<h3>' + el.thought + '</h3>' + '</li>');
+                    })
+            $(".tooltip").append('<h2> Recommendation: ' + recommendations(i) + '</h2>')
+          })
+          .on("mouseout", function(d) {
+              tooltip.transition()
+                   .duration(500)
+                   .style("opacity", 0);
+          });
+
         
     var text = svg.selectAll("text")
         .data(patternFrequencies)
@@ -127,31 +140,13 @@ var thoughts =  [
         
         // x and y need to equal cx and cy above
         .attr("x", (function(d, i) {
-            return ((300 * i) + (d * 10));
+            return ((50 * i) + (d * 10));
             }))
         .attr("y", (function(d, i) {
-            return d * 16;
+            return i * 150;
             }))
         .attr("dy", ".3em")
         .style("text-anchor", "middle")
         .text((function(d, i) {
             return patternNames(i);
             }));
-            
-              
-  /*  var thoughts = svg.selectAll("record")
-        .data(patternFrequencies)
-        .enter()
-        .append("text")
-        .text(function() {
-          _.each(thoughts, function(el, idx){
-            $(".title").append(el.thought_pattern);
-            $(".record").append('<li>' + '<h3>' + el.thought + '</h3>' + '</li>');
-            })
-        });
-  window.onload = function() {
-      _.each(thoughts, function(el, idx){
-        $(".title").append(el.thought_pattern);
-        $(".record").append('<li>' + '<h3>' + el.thought + '</h3>' + '</li>');
-        });
-    }; */
