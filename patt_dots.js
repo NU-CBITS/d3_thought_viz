c54e9afccbfe562bc875fe526451b4b01dcdb88e
gwensmu <gwen.smuda@gmail.com>
2014-05-20 16:24:01 -0500

    Revisions to layout

--- patt_dots.js
+++ patt_dots.js
@@ -12,7 +12,7 @@ var thoughts =  [
                         USER: '023342', 
                         id:2, 
                         thought:'I’ll never be good at playing bananagrams', 
-                        thought_pattern: 'Catastrophic',
+                        thought_pattern: 'Overgeneralization',
                         challenging_thought:'How do i know until I try?',
                         as_if:'I’m going to practice more'
                     },
@@ -28,66 +28,91 @@ var thoughts =  [
                     
                     {
                         USER:'023342', 
-                        id:3, 
+                        id:7, 
                         thought:'I’ll never be good at playing players', 
-                        thought_pattern: 'Falsehoods',
+                        thought_pattern: 'Minimization',
                         challenging_thought:'How do i know until I try?',
                         as_if:'I’m going to practice more'
                     },
                     
+                                        {
+                        USER:'023342', 
+                        id:7, 
+                        thought:'I’ll never be good at playing trombone', 
+                        thought_pattern: 'Mental Filter',
+                        challenging_thought:'How do i know until I try?',
+                        as_if:'I’m going to practice more'
+                    },
+                    
+                    
+                                        {
+                        USER:'023342', 
+                        id:7, 
+                        thought:'I’ll never be good at playing fiddle', 
+                        thought_pattern: 'Emotional Reasoning',
+                        challenging_thought:'How do i know until I try?',
+                        as_if:'I’m going to practice more'
+                    },
+                    
+                    
                     {
                         USER:'023342', 
-                        id:2, 
+                        id:6, 
                         thought:'I’ll never be good at playing guitar', 
-                        thought_pattern: 'Catastrophic',
+                        thought_pattern: 'Magnification',
                         challenging_thought:'How do i know until I try?',
                         as_if:'I’m going to practice more'
                     },
                     
                     {
                         USER:'023342', 
-                        id:2, 
+                        id:6, 
                         thought:'I’ll never be good at playing space invaders', 
-                        thought_pattern: 'Catastrophic',
+                        thought_pattern: 'Magnification',
                         challenging_thought:'How do i know until I try?',
                         as_if:'I’m going to practice more'
                     },
                  ];     
 
-    // thought patterns, to be replaced with actual pattern names
+    // thought patterns collections
     var allOrNothing = _.where(thoughts,{thought_pattern:'All-Or-Nothing Thinking'}).length;
-    var catastrophic = _.where(thoughts, {thought_pattern:'Catastrophic'}).length;
-    var falsehoods = _.where(thoughts, {thought_pattern:'Falsehoods'}).length;
+    var overgeneralization = _.where(thoughts, {thought_pattern:'Overgeneralization'}).length;
+    var mentalFilter = _.where(thoughts, {thought_pattern:'Mental Filter'}).length;
+    var disqualifying = _.where(thoughts, {thought_pattern:'Disqualifying the positive'}).length;
+    var jumpingToConclusions = _.where(thoughts, {thought_pattern:'Jumping to conclusions'}).length;
+    var magnification = _.where(thoughts, {thought_pattern:'Magnification'}).length;
+    var minimization = _.where(thoughts, {thought_pattern:'Minimization'}).length;
+    var emotionalReasoning = _.where(thoughts, {thought_pattern:'Emotional Reasoning'}).length;
+    var shouldStatements = _.where(thoughts, {thought_pattern:'Should Statements'}).length;
+    var labelingMislabeling = _.where(thoughts, {thought_pattern:'Labeling and Mislabeling'}).length;
+    var personalization = _.where(thoughts, {thought_pattern:'Personalizations'}).length;    
 
     // used to build dot size
-    var patternFrequencies = [parseInt(allOrNothing), parseInt(catastrophic), parseInt(falsehoods)];
-    
-    /* var frequency = d3.scale.ordinal()
-        .range = [parseInt(allOrNothing), parseInt(catastrophic), parseInt(falsehoods)]; */
-
+    var patternFrequencies = [parseInt(allOrNothing), parseInt(overgeneralization), parseInt(mentalFilter),
+                                parseInt(disqualifying), parseInt(jumpingToConclusions), parseInt(magnification),
+                                parseInt(minimization), parseInt(emotionalReasoning), parseInt(shouldStatements),
+                                parseInt(labelingMislabeling), parseInt(personalization)];
+                               
     // color of dot, index matches patternNames
     var color = d3.scale.ordinal()
-        .range(["#98abc5", "#8a89a6", "#7b6888"]);
+        .range(["#98abc5", "#8a89a6", "#7b6888", "#339966", "#996699", "#CC6666", "#9999FF", "#FFCC99", "#FFCCFF", "#6666FF", "#FF66000"]);
      
     // name of pattern to label dot and connect dot graphic to object collections on mouseover
     var patternNames = d3.scale.ordinal()
-        .range(['All-Or-Nothing Thinking', 'Catastrophic', 'Falsehoods']);
+        .range(['All-Or-Nothing Thinking', 'Overgeneralization', 'Mental Filter', 'Disqualifying the positive', 
+                    'Jumping to conclusions', 'Magnification', 'Minimization', 'Emotional Reasoning',
+                    'Should Statements', 'Labeling and Mislabeling', 'Personalization']);
         
     // clinician recommendation to be indexed here; need to revise if more than one recommendation per pattern
-    var recommendations = ['See shades of grey', 'Take a deep breath', 'Challenge your assumptions'];  
+    var recommendations = ['See shades of grey', 'Focus on specifics', 'See the big picture', 
+                            'Consider all options', 'Gather all the facts', 'Put it in context',
+                            'Its important', 'Is that your head or heart?', 'Shoulda coulda woulda', 
+                            'Is it so simple?', 'Is that yours to take on?'];  
         
     // margin of the SVG that contains dots
-    var margin = {top: 100, right: 30, bottom: 50, left: 200},
-        width = 600 - margin.left - margin.right,
-        height = 500 - margin.top - margin.bottom;
-        
-    /* add the record area to the webpage
-    var record = d3.select("body").append("ul")
-        .attr("class", "record")
-        
-     // add the title area to the webpage
-    var title = d3.select("body").append("h1")
-        .attr("class", "title") */
+    var margin = {top: 100, right: 10, bottom: 30, left: 30},
+        width = 800 - margin.left - margin.right,
+        height = 1200 - margin.top - margin.bottom;
         
     // add the mouseover details area to the webpage
     var tooltip = d3.select("body").append("div")
@@ -112,16 +137,16 @@ var thoughts =  [
         .attr("class", "dot")
         .enter().append("circle")
         .attr("r", (function(d) {
-            return d * 30;}))
+            return (d * 30) + 5;}))
         .style("fill", (function(d, i) {
             return color(i) }))
             
-         // cx and cy will affect dot position, variable based on dot size, will need to refactor for all 12 categories
+         // cx and cy will affect dot position, variable based on dot size
         .attr("cx", (function(d, i) {
-            return ((50 * i) + (d * 10));
+            return (40 * d) + 100;
             }))
         .attr("cy", (function(d, i) {
-            return i * 150;}))
+            return 120 * i;}))
         
         .on("mouseover", function(d, i) {
             tooltip.transition()
@@ -148,10 +173,10 @@ var thoughts =  [
         
         // x and y need to equal cx and cy above
         .attr("x", (function(d, i) {
-            return ((50 * i) + (d * 10));
+            return (40 * d) + 100;
             }))
         .attr("y", (function(d, i) {
-            return i * 150;
+            return i * 120;
             }))
         .attr("dy", ".3em")
         .style("text-anchor", "middle")
